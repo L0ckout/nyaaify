58e730472607158a5e207256571f32d8d1bf582a
L0ckout <67959046+L0ckout@users.noreply.github.com>
2020-10-22 10:51:02 -0500

    Nya!

--- index.js
+++ index.js
@@ -4,38 +4,38 @@ const { Plugin } = require('powercord/entities');
 const { getModule, React } = require('powercord/webpack');
 const { inject, uninject} = require('powercord/injector');
 
-function desufy(s){
+function nyaify(s){
         s = s.replace(/(.)([.?!~]?)(?:\n|$)/g,function(match,p1,p2,offset,string)
         {
-                return `${p1}${p1==='.'||p1===','?'':','} desu${p2||'.'}\n`
+                return `${p1}${p1==='.'||p1===','?'':','} nya${p2||'!'}\n`
         })
         return s
 }
 
 deus = false;
 
-module.exports = class desuifier extends Plugin {
+module.exports = class nyaifier extends Plugin {
 	async startPlugin () {
 	  powercord.api.commands.registerCommand({
-		command: 'desuauto',
-		description: 'make your desu auto. desu',
+		command: 'nyaauto',
+		description: 'make your nya auto. Nya!',
 		usage: '{c}',
 		executor: (args) => {
 			deus=!deus
 			return {
 		        send: false,
-			result: `Your text will now${deus?'':' not'} be desuified. desu.`
+			result: `Your text will now${deus?'':' not'} be nyad. Nya!`
 		}}
 	});
 	powercord.api.commands.registerCommand({
-		command: 'desu',
-		description: 'desuify a text. desu',
-		usage: '{c} [text to desu]',
+		command: 'nya',
+		description: 'nyaify a text. Nya!',
+		usage: '{c} [text to nya]',
 		executor: (args)=>{
 			let ret = args.join(' ')
 			if(!deus)
 			{
-				ret = desufy(ret)
+				ret = nyaify(ret)
 			}
 			return {
 			send:true,
@@ -47,7 +47,7 @@ module.exports = class desuifier extends Plugin {
     		inject("owoifierSend", messageEvents, "sendMessage", function(args) {
 	    	if(deus) {
         	let text = args[1].content;
-        	text = desufy(text);
+        	text = nyaify(text);
         	args[1].content = text;      
 	}      
 	return args;  
@@ -56,8 +56,8 @@ module.exports = class desuifier extends Plugin {
   }
  
   pluginWillUnload () {
-	powercord.api.commands.unregisterCommand('desuauto');
-	powercord.api.commands.unregisterCommand('desu');
+	powercord.api.commands.unregisterCommand('nyaauto');
+	powercord.api.commands.unregisterCommand('nya');
   }
 };
 
